bb15c4c668cfb7c8ac7443b5f5895ede85ce43c0
thm <2419741955@qq.com>
2024-02-08 10:49:21 +0800

    webpack

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,85 @@
-import React, { Suspense, FC } from "react";
+// import React, { Suspense, FC } from "react";
 import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import routerList from "./pages";
+import { Layout } from "antd";
+import SideBar from "./component/common/sideBar";
+
 export interface ModuleProps {
   name: string;
   routeProps: { path: string; element: React.ReactElement };
 }
 
+const { Header, Footer, Sider, Content } = Layout;
+
 const App: React.FC = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        {routerList.map((m: ModuleProps) => {
-          return (
-            <Route
-              path={m.routeProps.path}
-              element={m.routeProps.element}
-              key={m.name}
-            />
-          );
-        })}
-      </Routes>
-    </BrowserRouter>
+    <Layout style={layoutStyle}>
+      <Sider width="20%" style={siderStyle}>
+        <SideBar />
+      </Sider>
+      <Layout>
+        <Header style={headerStyle}>Header</Header>
+        <Content style={contentStyle}>
+          <BrowserRouter>
+            <Routes>
+              {routerList.map((m: ModuleProps) => {
+                return (
+                  <Route
+                    path={m.routeProps.path}
+                    element={m.routeProps.element}
+                    key={m.name}
+                  />
+                );
+              })}
+            </Routes>
+          </BrowserRouter>
+        </Content>
+        <Footer style={footerStyle}>Footer</Footer>
+      </Layout>
+    </Layout>
   );
 };
 
-// const App: React.FC = () => {
-//   return (
-//     <BrowserRouter>
-//       <Routes>
-//         {routerList.map((m: ModuleProps) => {
-//           return (
-//             <Route
-//               path={m.routeProps.path}
-//               element={m.routeProps.element}
-//               key={m.name}
-//             />
-//           );
-//         })}
-//       </Routes>
-//     </BrowserRouter>
-//   );
-// };
+const headerStyle: React.CSSProperties = {
+  textAlign: "center",
+  color: "#4096ff",
+  height: 64,
+  paddingInline: 48,
+  lineHeight: "64px",
+  backgroundColor: "#fff",
+  // backgroundColor: "#4096ff",
+};
+
+const contentStyle: React.CSSProperties = {
+  textAlign: "center",
+  minHeight: 120,
+  lineHeight: "120px",
+  color: "#0958d9",
+  // backgroundColor: "#0958d9",
+};
+
+const siderStyle: React.CSSProperties = {
+  textAlign: "center",
+  lineHeight: "120px",
+  color: "#1677ff",
+  backgroundColor: "#fff",
+  // backgroundColor: "#1677ff",
+};
+
+const footerStyle: React.CSSProperties = {
+  textAlign: "center",
+  color: "#fff",
+  backgroundColor: "#fff",
+  // backgroundColor: "#4096ff",
+};
+
+const layoutStyle = {
+  borderRadius: 8,
+  overflow: "hidden",
+  width: "calc(100% - 8px)",
+  maxWidth: "calc(100% - 8px)",
+  height: "100vh",
+};
+
 export default App;
---
+++ src/component/common/sideBar/index.tsx
@@ -0,0 +1,5 @@
+const SideBar: React.FC = () => {
+  return <li>11122222222222222222222</li>;
+};
+
+export default SideBar;
